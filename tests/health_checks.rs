2b9bf7af0a82e351f2b8d7ed34dc9049710fb8db
HerveH44 <herve.huneau@gmail.com>
2024-01-04 16:37:25 +0100

    rename test

--- tests/health_checks.rs
+++ tests/health_checks.rs
@@ -4,7 +4,7 @@ mod tests {
     use zero2prod::health_check;
 
     #[actix_web::test]
-    async fn test_health_check_get() {
+    async fn health_check_works() {
         let app = test::init_service(App::new().service(health_check)).await;
         let req = test::TestRequest::get().uri("/health_check").to_request();
         let resp = test::call_service(&app, req).await;

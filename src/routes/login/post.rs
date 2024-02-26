9219606bc23d6167c8c3a94e8f5c8ae870d92310
HerveH44 <herve.huneau@gmail.com>
2024-02-26 18:06:45 +0100

    10.6.3.2 Rejecting invalid credentials

--- src/routes/login/post.rs
+++ src/routes/login/post.rs
@@ -1,6 +1,43 @@
-use actix_web::{post, web, HttpResponse, Responder};
-use reqwest::header::LOCATION;
+use std::fmt;
+
+use actix_web::{post, web, HttpResponse, ResponseError};
+use reqwest::{header::LOCATION, StatusCode};
 use secrecy::Secret;
+use sqlx::PgPool;
+
+use crate::{
+    authentication::{validate_credentials, AuthError, Credentials},
+    routes::error_chain_fmt,
+};
+
+#[tracing::instrument(
+    name = "Login",
+    skip(form, pool),
+    fields(username=tracing::field::Empty, user_id=tracing::field::Empty)
+)]
+#[post("/login")]
+pub async fn login(
+    form: web::Form<LoginFormData>,
+    pool: web::Data<PgPool>,
+) -> Result<HttpResponse, LoginError> {
+    tracing::Span::current().record("username", &tracing::field::display(&form.0.username));
+    let credentials = Credentials {
+        username: form.0.username,
+        password: form.0.password,
+    };
+
+    let user_id = validate_credentials(credentials, &pool)
+        .await
+        .map_err(|e| match e {
+            AuthError::InvalidCredentials(_) => LoginError::AuthError(e.into()),
+            AuthError::UnexpectedError(_) => LoginError::UnexpectedError(e.into()),
+        })?;
+
+    tracing::Span::current().record("user_id", &tracing::field::display(&user_id));
+    Ok(HttpResponse::SeeOther()
+        .insert_header((LOCATION, "/"))
+        .finish())
+}
 
 #[derive(serde::Deserialize)]
 pub struct LoginFormData {
@@ -8,9 +45,25 @@ pub struct LoginFormData {
     password: Secret<String>,
 }
 
-#[post("/login")]
-pub async fn login(_form: web::Form<LoginFormData>) -> impl Responder {
-    HttpResponse::SeeOther()
-        .insert_header((LOCATION, "/"))
-        .finish()
+#[derive(thiserror::Error)]
+pub enum LoginError {
+    #[error("Authentication failed")]
+    AuthError(#[source] anyhow::Error),
+    #[error("Something went wrong")]
+    UnexpectedError(#[from] anyhow::Error),
+}
+
+impl fmt::Debug for LoginError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        error_chain_fmt(self, f)
+    }
+}
+
+impl ResponseError for LoginError {
+    fn status_code(&self) -> StatusCode {
+        match self {
+            LoginError::AuthError(_) => StatusCode::UNAUTHORIZED,
+            LoginError::UnexpectedError(_) => StatusCode::INTERNAL_SERVER_ERROR,
+        }
+    }
 }
